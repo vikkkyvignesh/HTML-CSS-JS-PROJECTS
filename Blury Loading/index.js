7e8391bc77e78cbe2900e8041a1518ffee48770d
Vignesh <139255159+vikkkyvignesh@users.noreply.github.com>
2025-01-03 10:30:29 +0530

    Add files via upload

---
+++ Blury Loading/index.js
@@ -0,0 +1,22 @@
+const txt = document.querySelector(".txt");
+const bg = document.querySelector("img");
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  txt.innerText = `${load}%`;
+  txt.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
